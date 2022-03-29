54e9c2e839c06a7cc51776142c84c96db52ad61b
Christian Herber dos Santos <christianh@PC-1013592.pmtga.dom>
2022-03-29 09:41:54 -0400

    create home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
-import Container from 'react-bootstrap/Container'
+import Link from 'next/link'
+import {Button, Row, Col} from 'react-bootstrap'
+import {FaCrosshairs} from 'react-icons/fa'
 
 export default function Home() {
   return (
     <>
-      <Container>
-        Home Page :)
-      </Container>
+      <Row className='mt-8 justify-content-center'>
+        <Col md={7} xs={12} className='text-center'>
+          <h1 className='fw-bolder text-custom-gray-darker mb-5 lh-base display-5'>
+            Comida saudável e gostosa direto na sua casa
+          </h1>
+          <Link href='/restaurants'>
+            <Button variant='custom-red' size='lg' className='text-white'>
+              <FaCrosshairs />
+              <span className='px-2 text-uppercase fw-bolder'>Encontrar Agora</span>
+            </Button>
+          </Link>
+        </Col>
+      </Row>
     </>
   )
 }
