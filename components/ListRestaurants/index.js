b619d615b0576dcd47e064aaae97b69f8b592016
Christian Herber dos Santos <christianh@PC-1013592.pmtga.dom>
2022-03-29 10:13:40 -0400

    create components and page to list restaurants

---
+++ components/ListRestaurants/index.js
@@ -0,0 +1,16 @@
+import { Row, Col } from "react-bootstrap"
+
+const ListRestaurants = () => {
+    return (
+        <>
+            <h3>Restaurantes</h3>
+            <Row>
+                <Col>
+                    Restaurantes...
+                </Col>
+            </Row>
+        </>
+    )
+}
+
+export default ListRestaurants
