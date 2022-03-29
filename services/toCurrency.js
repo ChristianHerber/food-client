68c61bf8458f391528e921ab564ffbd7a29b33e9
Christian Herber dos Santos <christianh@PC-1013592.pmtga.dom>
2022-03-29 10:21:28 -0400

    create service toCurrency

---
+++ services/toCurrency.js
@@ -0,0 +1,11 @@
+const toCurrency = () => {
+
+    const formatter = Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+    })
+
+    return formatter.format(value)
+}
+
+export default toCurrency
