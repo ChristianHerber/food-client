a00a51451ef2c62e47a1079dfcbb5a8e3651f502
Christian Herber dos Santos <christianh@PC-1013592.pmtga.dom>
2022-03-29 13:23:35 -0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost']
+  },
+  env: {
+    apiUrl: 'https://localhost:3000'
+  }
 }
 
 module.exports = nextConfig
