21182ba47dda5ac50481d1172d652366faf2aa78
Christian Herber dos Santos <christianh@PC-1013592.pmtga.dom>
2022-03-29 10:26:22 -0400

    create service truncateString

---
+++ services/truncateString.js
@@ -0,0 +1,9 @@
+const truncateString = (str, num) => {
+    if(str.length <= num) {
+        return str
+    } else {
+        return str.slice(0, num + '...')
+    }
+}
+
+export default truncateString
